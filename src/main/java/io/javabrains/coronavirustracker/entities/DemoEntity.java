5255a84c28a8ab4d50b6b607ec38a7dd039283f8
arfin-bs23 <hafizur.rahman@brainstation23.com>
2020-03-09 19:06:44 +0600

    wowow

--- src/main/java/io/javabrains/coronavirustracker/entities/DemoEntity.java
+++ src/main/java/io/javabrains/coronavirustracker/entities/DemoEntity.java
@@ -1,10 +1,17 @@
 package io.javabrains.coronavirustracker.entities;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class DemoEntity {
     @Id
     @GeneratedValue
--- src/main/java/io/javabrains/coronavirustracker/entities/Greetings.java
+++ src/main/java/io/javabrains/coronavirustracker/entities/Greetings.java
@@ -2,6 +2,7 @@ package io.javabrains.coronavirustracker.entities;
 
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Greetings {
     @Id
     @GeneratedValue
